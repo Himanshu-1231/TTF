ce3af1f33f7026eae4496bd8a0e6f26481b7ef3c
himanshu rana <himanshurajput@1016>
2024-01-23 13:31:56 +0530

    uploading todays' file

--- javascript/fundamental.js
+++ javascript/fundamental.js
@@ -1,116 +1,116 @@
-/*Date- 16jan,2024
- * js intro
- link js to html
- js history
- statement
- js engine 
- run time env
- client - server architecture
- request response architecture
- comment
- semicolon
- case sensitivity
- native code of js
+// /*Date- 16jan,2024
+//  * js intro
+//  link js to html
+//  js history
+//  statement
+//  js engine 
+//  run time env
+//  client - server architecture
+//  request response architecture
+//  comment
+//  semicolon
+//  case sensitivity
+//  native code of js
 
 
- * 
- * 
- * '
- * 
- */
+//  * 
+//  * 
+//  * '
+//  * 
+//  */
 
-    // console.log("hello world !")
+//     // console.log("hello world !")
 
-//hello i am learning js  and its my first day
+// //hello i am learning js  and its my first day
 
-                        /**
-                         * this is my first multiline
-                         * comment
-                         * hello
-                         * world
-                         * 
-                         * 
-                         */
-
-
-                        /* 
-                        console.log("hi");
+//                         /**
+//                          * this is my first multiline
+//                          * comment
+//                          * hello
+//                          * world
+//                          * 
+//                          * 
+//                          */
 
 
-                        console.log("hi");
-
-                        console.log("hi");
-
-                        console.log("hi");
-
-                        console.log("hi");
-
-                        console.log("hi");
-                        */
+//                         /* 
+//                         console.log("hi");
 
 
-/***
- * output
- * js native output, browser output
- * variables
- * consitionals
- * loops
- * 
- * 
- * 
- * 
- */
+//                         console.log("hi");
+
+//                         console.log("hi");
+
+//                         console.log("hi");
+
+//                         console.log("hi");
+
+//                         console.log("hi");
+//                         */
 
 
-                    // js- variable -dynamically types- 
-                    var name="vikas"
-                    var age=27
-                    var height=6.2
-                    var weight=75.50
-                    var grade='A'
-
-                    console.log("my name is ",name)
-                    console.log("my age is ",age);
-
-                    console.log(`my name is ${name} and my age is ${age}`); //template string
-                    var myintrostring=`my name is vikas
-                    my age is 27
-                    i am a full stack web developer
-                    my tech stack is c+ .net mvc
-                    `
-                    console.log(myintrostring)
-                    // document.write(myintrostring)
+// /***
+//  * output
+//  * js native output, browser output
+//  * variables
+//  * consitionals
+//  * loops
+//  * 
+//  * 
+//  * 
+//  * 
+//  */
 
 
-    //conditionals
-    //if else else if switch ternary
+//                     // js- variable -dynamically types- 
+//                     var name="vikas"
+//                     var age=27
+//                     var height=6.2
+//                     var weight=75.50
+//                     var grade='A'
 
-//     var age=27
-//    if(age<18){
-//     console.log("you can vote ")
+//                     console.log("my name is ",name)
+//                     console.log("my age is ",age);
 
-//    }else{
-//     console.log("hello world")
+//                     console.log(`my name is ${name} and my age is ${age}`); //template string
+//                     var myintrostring=`my name is vikas
+//                     my age is 27
+//                     i am a full stack web developer
+//                     my tech stack is c+ .net mvc
+//                     `
+//                     console.log(myintrostring)
+//                     // document.write(myintrostring)
 
-//    }
-//    console.log("outside if else") -->
+
+//     //conditionals
+//     //if else else if switch ternary
+
+// //     var age=27
+// //    if(age<18){
+// //     console.log("you can vote ")
+
+// //    }else{
+// //     console.log("hello world")
+
+// //    }
+// //    console.log("outside if else") -->
 
 
 
-                //   var age=25;
+//                 //   var age=25;
 
-                //   if(age<18)
-                //     {
-                //      console.log("go to school");
-                //    }else{
-                //      if(age>18 && age<20){
-                //          console.log("go to college");
-                //      }else{
-                //          if(age>20 && age<30){
+//                 //   if(age<18)
+//                 //     {
+//                 //      console.log("go to school");
+//                 //    }else{
+//                 //      if(age>18 && age<20){
+//                 //          console.log("go to college");
+//                 //      }else{
+//                 //          if(age>20 && age<30){
 
-                //          }
-                //      }
-                //   }
+//                 //          }
+//                 //      }
+//                 //   }
 
 
                   
@@ -118,18 +118,71 @@
 
 
 
-var age = 35;
-if(age<18){
-    console.log("go to school")
-}else if(age>18 && age<20){
-    console.log("go to college")
-}else if(age>20 && age<30){
-    console.log("go to office")
-}else if(age>30 && age<40){
-    console.log("party")
-}else if(age>40 && age<50){
-    console.log("go to mandir")
-}
-else{
-    console.log("stay at home")
+// var age = 35;
+// if(age<18){
+//     console.log("go to school")
+// }else if(age>18 && age<20){
+//     console.log("go to college")
+// }else if(age>20 && age<30){
+//     console.log("go to office")
+// }else if(age>30 && age<40){
+//     console.log("party")
+// }else if(age>40 && age<50){
+//     console.log("go to mandir")
+// }
+// else{
+//     console.log("stay at home")
+// }
+
+
+
+// ----------------------------------------------------------------
+
+
+// entry control loop vs exit control loop
+
+// ------------------------------------------------------
+
+
+// let num = 7;
+// let count = 0;
+// for (let div=2;div<=num;div++)
+// {
+//     if (num%div==0)
+//     {
+//         count++;
+
+//     }
+// }
+// if(count>2)
+// {
+//     console.log("not prime ",num);
+// }
+// else 
+// {
+//     console.log("prime ",num)
+// }
+
+
+let ll=10;
+let ul=100;
+
+for(let num=ll;num<ul;num++)
+{
+    let count =0;
+    for(let div=2;div<=num-1;div++)
+    {
+       if(num%div == 0)
+       {
+        count++;
+        break;
+       }
+    }
+    if(count>0)
+    {
+        console.log("not prime ",num);
+    }
+    else{
+        console.log("prime ",num);
+    }
 }
